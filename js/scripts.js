0102cb1ba07a7d536d8c50b6e85981f6b498a8a1
Lisa Walters <lisacampiwalters678@gmail.com>
2021-06-27 22:54:53 -0600

    Week 4 Assigment

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
-<script>
-        $(function () {
-            $(".carousel").carousel({ interval: 2000 });
-            $("#carouselButton").click(function () {
-                if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-                    $(".carousel").carousel("pause");
-                    $("#carouselButton").children("i").removeClass("fa-pause");
-                    $("#carouselButton").children("i").addClass("fa-play");
-                } else {
-                    $(".carousel").carousel("cycle");
-                    $("#carouselButton").children("i").removeClass("fa-play");
-                    $("#carouselButton").children("i").addClass("fa-pause");
-                }
-            });
-        });
-    </script>
+
+
+
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });//end click function
+
+    //Here is where we activate the Reserve Campsite and Login modal
+
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+
+
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
+
+});//end jquery ready function
+
